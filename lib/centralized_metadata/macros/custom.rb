172a0c20aa1340405c938a29beb7c3ea99f0cb90
sensei100 <tuh42082@temple.edu>
2022-08-05 10:45:27 -0400

    Updates names and spacing issues, also sets sqlite3 as adaptor for dev and test environments

--- lib/centralized_metadata/macros/custom.rb
+++ lib/centralized_metadata/macros/custom.rb
@@ -6,6 +6,7 @@ module CentralizedMetadata::Macros::Custom
   def extract_original_key
     lambda do |rec, acc|
       field = rec["384"]
+
       if field&.indicator1 == "0" || field&.indicator1 == " "
         field.subfields.each { |sf| acc << sf.value if sf.code == "a" }
       end
@@ -15,6 +16,7 @@ module CentralizedMetadata::Macros::Custom
   def extract_work_time_creation
     lambda do |rec, acc|
       field = rec["388"]
+
       if field&.indicator1 == "1" || field&.indicator1 == " "
         field.subfields.each { |sf| acc << sf.value if sf.code == "a" }
       end
@@ -35,15 +37,16 @@ module CentralizedMetadata::Macros::Custom
 
   def set_type
     lambda do |rec, acc|
-
       case
       when rec["100"]&.indicator1 != "3" &&
-          rec["100"]&.subfields&.none? { |sf| sf.code == "t" }
+        rec["100"]&.subfields&.none? { |sf| sf.code == "t" }
+        
         acc << "personal name"
 
       when rec["100"]&.indicator1 == "3" &&
         rec["100"]&.subfields&.none? { |sf| sf.code == "t" }
-      acc << "family name"
+      
+        acc << "family name"
 
       when  rec["110"]&.subfields&.none? { |sf| sf.code == "t" }
         acc << "corporate name"
@@ -54,6 +57,7 @@ module CentralizedMetadata::Macros::Custom
       when  rec["100"]&.subfields&.any? { |sf| sf.code == "t" } ||
         rec["110"]&.subfields&.any? { |sf| sf.code == "t" }  ||
         rec["111"]&.subfields&.any? { |sf| sf.code == "t" }
+        
         acc << "name-title work"
       
       when rec["130"]
@@ -81,13 +85,13 @@ module CentralizedMetadata::Macros::Custom
     lambda do |rec, acc|
       Traject::MarcExtractor.cached("500abcdfghjklmnopqrstv:510abcdfghjklmnoprstv:511acdefghklnpqstv:530adfghklmnoprstv:547acdgv:548av:550abgj:551agv:555av").collect_matching_lines(rec) do |field, spec, extractor|
         see_also_datafields = rec.fields.select { |f| f if extractor.interesting_tag?(f.tag) }
-        select_datafields = field if see_also_datafields.include?(field)
+        selected_datafields = field if see_also_datafields.include?(field)
       
-        if select_datafields&.subfields&.none? { |sf| sf.code == "w" } ||
-          select_datafields&.any? { |f| f.code == "w" && !f.value&.start_with?("g") } ||
-          select_datafields&.any? { |f| f.code == "w" && !f.value&.start_with?("h") }
+        if selected_datafields&.subfields&.none? { |sf| sf.code == "w" } ||
+          selected_datafields&.any? { |f| f.code == "w" && !f.value&.start_with?("g") } ||
+          selected_datafields&.any? { |f| f.code == "w" && !f.value&.start_with?("h") }
           
-          acc << extractor.collect_subfields(select_datafields, spec).first
+          acc << extractor.collect_subfields(selected_datafields, spec).first
         end
       end  
     end
@@ -96,12 +100,12 @@ module CentralizedMetadata::Macros::Custom
   def extract_narrower_term
     lambda do |rec, acc|
       Traject::MarcExtractor.cached("500abcdfghjklmnopqrstv:510abcdfghjklmnoprstv:511acdefghklnpqstv:530adfghklmnoprstv:547acdgv:548av:550abgj:551agv:555av").collect_matching_lines(rec) do |field, spec, extractor|
-        see_also_datafields = rec.fields.select { |f| f if extractor.interesting_tag?(f.tag) }
-        select_datafields = field if see_also_datafields.include?(field)
+        narrower_term_datafields = rec.fields.select { |f| f if extractor.interesting_tag?(f.tag) }
+        selected_datafields = field if narrower_term_datafields.include?(field)
         
-        if select_datafields&.any? { |f| f.code == "w" && f.value&.start_with?("h") }
+        if selected_datafields&.any? { |f| f.code == "w" && f.value&.start_with?("h") }
 
-          acc << extractor.collect_subfields(select_datafields, spec).first
+          acc << extractor.collect_subfields(selected_datafields, spec).first
         end
       end  
     end
