6d68fa09a759eec591f04522216648edf7297c3b
David Kinzer <dtkinzer@gmail.com>
2022-12-01 11:31:13 -0500

    Remove unused migration.

--- db/migrate/20221201152830_add_local_metadatum_id_to_record.rb
+++
@@ -1,6 +0,0 @@
-class AddLocalMetadatumIdToRecord < ActiveRecord::Migration[7.0]
-  def change
-    add_column :records, :local_metadatum_id, :integer
-    add_index :records, :local_metadatum_id
-  end
-end
