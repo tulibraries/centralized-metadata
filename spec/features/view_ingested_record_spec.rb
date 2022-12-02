f4871e7bcc7955700502eda2107e60cc1cec5ee6
David Kinzer <dtkinzer@gmail.com>
2022-12-02 14:09:10 -0500

    Only use serilizer to change record in show view.

--- spec/features/view_ingested_record_spec.rb
+++ spec/features/view_ingested_record_spec.rb
@@ -9,14 +9,14 @@ RSpec.feature "ViewIngestedRecords", type: :feature do
   scenario "go to the records page" do
     visit "/records"
 
-    json = JSON.parse(html)
-    expect(json.count).to eq(1)
-    expect(json.first["value"]["cm_id"].join("")).to eq("2043308")
+    results = JSON.parse(html)
+    expect(results.count).to eq(1)
+    expect(results.first["value"]["cm_id"].join("")).to eq("2043308")
   end
 
   scenario "go to a specific record" do
     visit "/records/2043308"
-    json = JSON.parse(html)
-    expect(json["cm_id"].join("")).to eq("2043308")
+    object = JSON.parse(html)
+    expect(object["value"]["cm_id"].join("")).to eq("2043308")
   end
 end
