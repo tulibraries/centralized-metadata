bb189a4503da9ac0776ac2727d625c119f2907e6
sensei100 <tuh42082@temple.edu>
2025-04-04 13:22:24 -0400

    Update autoload paths

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,9 @@ module CentralizedMetadata
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w[assets tasks])
+    Rails.application.config.autoload_paths += %W(#{Rails.root}/lib)
+    Rails.application.config.autoload_paths -= %W(#{Rails.root}/app/assets)
+    Rails.application.config.autoload_paths -= %W(#{Rails.root}/app/tasks)
 
     # Configuration for the application, engines, and railties goes here.
     #
