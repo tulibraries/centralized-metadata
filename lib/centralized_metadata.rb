4e33f4e9c36c37d1fb99158bf7566dbe31fbf2f0
sensei100 <tuh42082@temple.edu>
2026-05-06 15:03:59 -0400

    Remove autoloads

--- lib/centralized_metadata.rb
+++ lib/centralized_metadata.rb
@@ -1,7 +1,4 @@
 # frozen_string_literal: true
 
 module CentralizedMetadata
-  autoload :Indexer, "centralized_metadata/indexer"
-  autoload :ActiveRecordWriter, "centralized_metadata/active_record_writer"
-  autoload :Macros, "centralized_metadata/macros"
 end
