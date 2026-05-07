d05b1c5cbe8ce03d3d99e6c18880f29c80b9dee3
sensei100 <tuh42082@temple.edu>
2026-05-07 14:49:30 -0400

    Add indexer_config file to autoload ignore

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,11 @@ module CentralizedMetadata
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w[assets tasks])
+    config.autoload_lib(ignore: %w[
+      assets
+      centralized_metadata/indexer_config.rb
+      tasks
+    ])
 
     # Dart Sass builds stylesheets into app/assets/builds for Propshaft to serve.
     config.assets.excluded_paths << Rails.root.join("app/assets/stylesheets")
